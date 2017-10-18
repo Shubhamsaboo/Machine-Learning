954c27d9692df676cdfd5587299d54f37da586ca
Shubham Saboo <31396011+Shubhamsaboo@users.noreply.github.com>
2017-10-18 19:32:52 +0530

    Update Support_Vector_Machine(Code).R

--- Credit Risk Assesment Model/Support_Vector_Machine(Code).R
+++ Credit Risk Assesment Model/Support_Vector_Machine(Code).R
@@ -1,11 +1,16 @@
 #Reading Data
 df <- read.csv('loan_data.csv')
+
 #Converting to factors
 df$inq.last.6mths <- factor(df$inq.last.6mths)
 df$delinq.2yrs <- factor(df$delinq.2yrs)
 df$pub.rec <- factor(df$pub.rec)
 df$not.fully.paid <- factor(df$not.fully.paid)
 df$credit.policy <- factor(df$credit.policy)
+
+#FICO score is a type of credit score created by the 'Fair Isaac Corporation'.Lenders use borrowers
+#FICO scores along with other details on borrowers credit reports to assess credit risk and 
+#determine whether to extend credit.
 
 ###Exploratory Data Analysis
 library(ggplot2)
@@ -19,21 +24,21 @@ ggplot(df, aes(x = factor(purpose))) + geom_bar(aes(fill= not.fully.paid), posit
 #Scatterplot of fico score versus int.rate
 ggplot(df, aes(x = int.rate, y = fico)) + geom_point(aes(color = not.fully.paid)) + theme_bw()
 
-#Building the Model
-
 #Splitting the Data
 library(caTools)
 sample <- sample.split(df, SplitRatio = 0.7)
 train <- subset(df, sample == T)
 test <- subset(df, sample == F)
 
+########Model(Support Vector Machines)
 library(e1071)
-model <- svm(not.fully.paid ~. ,data = train)
-summary(model)
-predicted.values <- predict(model , test[1:13])
-table(predicted.values, test$not.fully.paid)
-#####Tuning Svm
+
+#Training Model
 tuned.svm <- tune(svm, train.x = not.fully.paid~., data = train , kernel= 'radial',ranges = list(cost= c(1,10,100)) ,gamma =c(0.1,.2,.3,1,10) )
+
+#Prediction 
 tuned.model <- svm(not.fully.paid ~ .,data=train,cost=10,gamma = 0.1)
 predicted.values <- predict(tuned.model,test[1:13])
+
+#Model Evaluation
 table(predicted.values,test$not.fully.paid)
