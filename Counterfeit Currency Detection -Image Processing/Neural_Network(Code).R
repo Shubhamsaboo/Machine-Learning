e6914fd4f4c21c96fa4e9bcc572489b8c1803abe
Shubham Saboo <31396011+Shubhamsaboo@users.noreply.github.com>
2017-10-18 19:05:41 +0530

    Update Neural_Network(Code).R

--- Counterfeit Currency Detection -Image Processing/Neural_Network(Code).R
+++ Counterfeit Currency Detection -Image Processing/Neural_Network(Code).R
@@ -1,50 +1,42 @@
 ##Reading Data
 df <- read.csv('bank_note_data.csv')
-##EDA
+
+##Exploratory Data Analysis
 library(ggplot2)
 ggplot(df, aes(x = factor(Class))) + geom_bar(aes(fill= factor(Class)))
 ggplot(df, aes(x= Image.Skew, y = Image.Curt)) + geom_point(aes(color= factor(Class))) +theme_dark()
 ggplot(df, aes(x= Image.Var, y = Image.Curt)) + geom_point(aes(color= factor(Class))) +theme_dark()
-#######
+
+#######Calling Necessary Libraries
 library(caTools)
 library(neuralnet)
 #####
-#Splitting the data
+
+#Splitting the data(Using catools lib)
 sample <- sample.split(df, SplitRatio = 0.7)
 train <- subset(df,sample==T)
 test <- subset(df, sample==F)
 
-###Neural n/w
-
+#Training Model On Train Dataset
+###Neural n/w Model
 nn <- neuralnet(formula = Class ~ Image.Var+ Image.Skew+ Image.Curt+ Entropy, data = train, hidden = 10,linear.output = F)
 
-#PREDICTION
+#PREDICTION (Test Dataset)
 predict.class <-compute(nn,test[1:4])
 
 #Rounding off to get class 1 and 0
+#Final predicted output that shows whether the Currency is genuine or not....
 predict <- apply(predict.class$net.result,1,round)
 
-##Mse 
+###############
+#EVALUATING TRAINED MODEL 
+
+##Mean Square Error(MSE) 
 MSE.nn <- sum((test$Class - predict)^2)/nrow(test)
 
-#confusion matrix of your predictions versus the real values
+#Confusion matrix of your predictions versus the real values
 table(predict, test$Class)
 
-###Comparing with randomforest
-library(randomForest)
-df$Class <- factor(df$Class)
-##Splitting Data
-sample <- sample.split(df, SplitRatio = 0.7)
-train <- subset(df,sample==T)
-test <- subset(df, sample==F)
-
-##Training model 
-rf<- randomForest(Class ~. , data = train)
-#prediction
-predict.val <- predict(rf, test[1:4]) 
-
-###Confusion Matrix
-table(predict.val, test$Class)
 
 
 
